153b8d671d86c4e752ec2b0fefea8dcc96622877
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2024-03-24 14:16:56 +0200

    Add Chris Doty-Humphrey's Small Fast Chaotic PRNG as a bitgenerator.
    This is a port of numpy's random.SFC64 implementation.

---
+++ lib/bitgen.ml
@@ -0,0 +1,13 @@
+open Stdint
+
+module type S = sig
+    type t 
+    val next_uint64 :  t -> uint64 * t
+    val next_uint32 : t -> uint32 * t
+    val next_double : t -> float * t
+    val initialize : int -> t
+end
+
+
+module SFC64 = Sfc.SFC64
+module SeedSequence = Seed.SeedSequence
