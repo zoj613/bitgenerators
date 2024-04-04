03e707633d914774c23153fc0a35c757cac21b96
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2024-04-05 00:42:37 +0200

    Add SeedSequence module and corresponding tests.
    This commit adds an implementation that is a port of numpy's
    random.SeedSequence object. The ideas behind it orginate from
    https://www.pcg-random.org/posts/developing-a-seed_seq-alternative.html
    whose's reference implementation is https://gist.github.com/imneme/540829265469e673d045

---
+++ lib/seed.ml
@@ -0,0 +1,200 @@
+open Stdint
+
+(* Return an integer with 128 random bits by combining 2 integers with 64 random bits. *)
+let randbits128 () =
+    let x = Int64.abs (Random.bits64 ())
+    and y = Int64.abs (Random.bits64 ()) in
+    Uint128.(logor (shift_left (of_int64 x) 64) (of_int64 y))
+
+
+let xshift = Ctypes.sizeof Ctypes_static.uint32_t * 8 / 2
+let mult_a = Uint32.of_int 0x931e8875
+
+let hashmix value const =
+    let open Uint32 in
+    let value = logxor value const
+    and const = const * mult_a in
+    let value = value * const in
+    let value = logxor (shift_right value xshift) value in
+    value, const
+
+
+let mix_mult_l = Uint32.of_int 0xca01f9dd
+let mix_mult_r = Uint32.of_int 0x4973f715
+
+let mix x y =
+    let res = Uint32.(mix_mult_l * x - mix_mult_r * y) in
+    Uint32.(logxor (shift_right res xshift) res)
+
+
+let mask32 = Uint128.of_int 0xFFFFFFFF
+(* Convert an unsigned 128bit integer into a list of unsigned 32 bit integers *)
+let u128_to_u32_list n =
+    let rec loop i acc =
+        if i <= Uint128.zero then
+            (* reverse the output list to ensure correct order *)
+            if List.is_empty acc then [Uint32.zero] else List.rev acc
+        else
+            let u32 = Uint32.of_uint128 (Uint128.logand i mask32) in
+            loop Uint128.(shift_right i 32) (u32 :: acc)
+    in
+    loop n []
+
+
+let to_uint32_list x =
+    List.(map u128_to_u32_list x |> concat)
+
+
+module SeedSequence : sig
+    type t
+    val initialize : ?spawn_key:int list -> ?pool_size:int -> uint128 list -> t
+    val generate_32bit_state : int -> t -> uint32 array
+    val generate_64bit_state : int -> t -> uint64 array
+    val spawn : int -> t -> t list * t
+    val children_spawned : t -> int
+    val entropy : t -> uint128 list
+end = struct
+
+    type t = {
+        entropy : uint128 list;
+        spawn_key : uint128 list;
+        children_spawned : int;
+        pool : uint32 array;
+    }
+
+    let entropy t = t.entropy
+
+    let children_spawned t = t.children_spawned
+
+    let init_a = Uint32.of_int 0x43b0d7e5
+
+    let mix_entropy entropy pool_size = 
+        let rec loop i mixer const =
+            if i >= pool_size then
+                mixer, const
+            else if i < Array.length entropy then
+                let v, const' = hashmix entropy.(i) const in
+                mixer.(i) <- v;
+                loop (i + 1) mixer const'
+            else
+                let v, const' = hashmix Uint32.zero const in
+                mixer.(i) <- v;
+                loop (i + 1) mixer const'
+        in
+        let mixer, const = loop 0 (Array.make pool_size Uint32.zero) init_a
+        in
+        let rec inner i k mixer const =
+            if i >= pool_size then
+                mixer, const
+            else if i = k then
+                inner (i + 1) k mixer const
+            else
+                let v, const' = hashmix mixer.(k) const in
+                mixer.(i) <- mix mixer.(i) v;
+                inner (i + 1) k mixer const'
+        in
+        let rec outer j mixer const =
+            if j >= pool_size then
+                mixer, const
+            else
+                let mixer', const' = inner 0 j mixer const in
+                outer (j + 1) mixer' const'
+        in
+        let mixer, const = outer 0 mixer const
+        in
+        let rec inner i k mixer const =
+            if i >= pool_size then
+                mixer, const
+            else
+                let v, const' = hashmix entropy.(k) const in
+                mixer.(i) <- mix mixer.(i) v;
+                inner (i + 1) k mixer const'
+        in
+        let outer j mixer const =
+            if j >= Array.length entropy then
+                mixer, const
+            else
+                let mixer', const' = inner 0 j mixer const in
+                outer (j + 1) mixer' const'
+        in
+        outer pool_size mixer const |> fst
+
+
+    let assembled_entropy entropy spawn_key pool_size =
+        let run_entropy = to_uint32_list entropy
+        and spawn_entropy = to_uint32_list spawn_key in
+        if List.length spawn_entropy > 0 && List.length run_entropy < pool_size then
+            let d = pool_size - List.length run_entropy in
+            [run_entropy; List.init d (fun _ -> Uint32.zero); spawn_entropy]
+            |> List.concat
+            |> Array.of_list
+        else
+            [run_entropy; spawn_entropy] |> List.concat |> Array.of_list
+
+
+    let initialize ?(spawn_key=[]) ?(pool_size=4) entropy =
+        (* 128 bits of system entropy are used when entropy is not provided *)
+        let entropy' = match entropy with
+            | [] -> [randbits128 ()]
+            | v -> v
+        and spawn_key' = List.map Uint128.of_int spawn_key in
+        let assembled = assembled_entropy entropy' spawn_key' pool_size in
+        {entropy = entropy';
+         spawn_key = spawn_key';
+         pool = mix_entropy assembled pool_size;
+         children_spawned = 0}
+
+
+    let spawn n t =
+        let rec loop i acc =
+            if i >= (t.children_spawned + n) then
+                acc
+            else
+                let spawn_key = t.spawn_key @ [Uint128.of_int i] in
+                let pool_size = Array.length t.pool in
+                let e = assembled_entropy t.entropy spawn_key pool_size in
+                let v = {t with spawn_key = spawn_key;
+                         pool = mix_entropy e pool_size;
+                         children_spawned = 0} in
+                loop (i + 1) (v::acc)
+        in
+        loop t.children_spawned [],
+        {t with children_spawned = t.children_spawned + n}
+
+
+    let init_b = Uint32.of_int 0x8b51f9dd
+    let mult_b = Uint32.of_int 0x58f38ded
+
+    let generate_32bit_state n_words t =
+        (* Get next value of a cyclic sequence, aka cycling over a list of values. *)
+        let next c = Seq.uncons c |> Option.get in
+        let rec loop i state cycle const =
+            if i >= n_words then
+                List.rev state |> Array.of_list
+            else
+                let data, cycle' = next cycle in
+                let data = Uint32.(logxor data const) in
+                let const = Uint32.(const * mult_b) in
+                let data = Uint32.(data * const) in
+                let data = Uint32.(logxor (shift_right data xshift) data) in
+                loop (i + 1) (data::state) cycle' const
+        in
+        loop 0 [] (Array.to_seq t.pool |> Seq.cycle) init_b
+
+
+    let generate_64bit_state n_words t =
+        (* 64 bit state array is obtained by combining elements i and (i + 1)
+           of the out32 to create a 64 bit integer using the formula
+           out32.(k + 1) << 32 | out32.(k), where k = i * 2 for i = 0, 1, ..., N
+           for N = length of out32 array. *)
+        let o = generate_32bit_state (n_words * 2) t |> Array.map Uint64.of_uint32 in
+        let rec to_uint64_array i state =
+            if i >= n_words then
+                List.rev state |> Array.of_list
+            else
+                let k = i * 2 in
+                let v = Uint64.logor (Uint64.shift_left o.(k + 1) 32) o.(k) in
+                to_uint64_array (i + 1) (v::state)
+        in
+        to_uint64_array 0 []
+end
---
+++ test/test_seedseq.ml
@@ -0,0 +1,90 @@
+open OUnit2
+open Stdint
+
+
+let inputs = [
+    [3735928559; 195939070; 229505742; 305419896];
+    [3668361503; 4165561550; 1661411377; 3634257570];
+    [164546577; 4166754639; 1765190214; 1303880213];
+    [446610472; 3941463886; 522937693; 1882353782];
+    [1864922766; 1719732118; 3882010307; 1776744564];
+    [4141682960; 3310988675; 553637289; 902896340];
+    [1134851934; 2352871630; 3699409824; 2648159817];
+    [1240956131; 3107113773; 1283198141; 1924506131];
+    [2669565031; 579818610; 3042504477; 2774880435];
+    [2766103236; 2883057919; 4029656435; 862374500];
+]
+
+let outputs32 = [
+    [3914649087; 576849849; 3593928901; 2229911004];
+    [2240804226; 3691353228; 1365957195; 2654016646];
+    [3562296087; 3191708229; 1147942216; 3726991905];
+    [1403443605; 3591372999; 1291086759; 441919183];
+    [1086200464; 2191331643; 560336446; 3658716651];
+    [3249937430; 2346751812; 847844327; 2996632307];
+    [2584285912; 4034195531; 3523502488; 169742686];
+    [959045797; 3875435559; 1886309314; 359682705];
+    [3978441347; 432478529; 3223635119; 138903045];
+    [296367413; 4262059219; 13109864; 3283683422];
+]
+
+let outputs64 = [
+    ["2477551240072187391"; "9577394838764454085"];
+    ["15854241394484835714"; "11398914698975566411"];
+    ["13708282465491374871"; "16007308345579681096"];
+    ["15424829579845884309"; "1898028439751125927"];
+    ["9411697742461147792"; "15714068361935982142"];
+    ["10079222287618677782"; "12870437757549876199"];
+    ["17326737873898640088"; "729039288628699544"];
+    ["16644868984619524261"; "1544825456798124994"];
+    ["1857481142255628931"; "596584038813451439"];
+    ["18305404959516669237"; "14103312907920476776"];
+]
+
+(* test_reference_data*bit functions test the output against that of the
+   reference implementation in C++ (https://gist.github.com/imneme/540829265469e673d045)
+   as well as that of numpy's numpy.random.SeedSequence *)
+let test_against_reference _ =
+    let open Bitgen.SeedSequence in
+    let gen_state32 n x =
+        List.map Uint128.of_int x
+        |> initialize
+        |> generate_32bit_state n
+        |> Array.map Uint32.to_int
+        |> Array.to_list
+    and gen_state64 n x =
+        List.map Uint128.of_int x
+        |> initialize
+        |> generate_64bit_state n
+        |> Array.map Uint64.to_string
+        |> Array.to_list
+    in
+    assert_equal
+        (List.concat outputs64)
+        (List.map (gen_state64 2) inputs |> List.concat)
+        ~printer:(List.fold_left (fun acc x -> acc ^ x ^ ", ") "");
+    assert_equal
+        (List.concat outputs32)
+        (List.map (gen_state32 4) inputs |> List.concat)
+        ~printer:(List.fold_left (fun acc x -> acc ^ string_of_int x ^ ", ") "")
+
+
+let test_zero_padding _ =
+    (* Ensure that large integers are inserted in little-endian fashion to avoid  trailing 0s.*)
+    let open Bitgen.SeedSequence in
+    assert_equal
+        (generate_32bit_state 4 (initialize [Uint128.of_int 42]))
+        (generate_32bit_state 4 (initialize [42 lsl 32 |> Uint128.of_int]))
+        ~cmp:(fun x y -> x <> y)
+
+
+let test_spawn_children _ =
+    let open Bitgen.SeedSequence in
+    assert_equal (initialize [] |> spawn 10 |> fst |> List.length) 10
+
+
+let tests = [
+    "Test SeedSequence output values against reference data" >:: test_against_reference;
+    "test correct numnber of spawn children" >:: test_spawn_children;
+    "test zero padding output" >:: test_zero_padding
+]
