4e6611f951ffca182845581dee28ce85dda3be41
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2024-04-15 18:31:14 +0200

    Add more unittests

--- test/test_philox.ml
+++ test/test_philox.ml
@@ -15,8 +15,8 @@ let test_counter_init _ =
     let open Stdint in
     let ss = SeedSequence.initialize [Uint128.of_int 12345] in
     let next_int init =
-        Philox.initialize_ctr ~counter:init ss 
-        |> Philox.next_uint64 |> fst |> Uint64.to_string
+        Philox64.initialize_ctr ~counter:init ss 
+        |> Philox64.next_uint64 |> fst |> Uint64.to_string
     in
     let base = next_int Uint64.(max_int, max_int, max_int, max_int) in
     assert_bool "" (base <> next_int Uint64.(max_int, max_int, max_int, zero));
@@ -28,9 +28,9 @@ let test_counter_init _ =
 let test_jump _ =
     let open Stdint in
     let ss = SeedSequence.initialize [] in
-    let t = Philox.initialize_ctr ~counter:Uint64.(max_int, max_int, max_int, max_int) ss |> Philox.jump in
-    let t' = Philox.initialize_ctr ~counter:Uint64.(max_int, max_int, zero, max_int) ss |> Philox.jump in
-    assert_bool "" ((Philox.next_double t |> fst) <> (Philox.next_double t' |> fst))
+    let t = Philox64.initialize_ctr ~counter:Uint64.(max_int, max_int, max_int, max_int) ss |> Philox64.jump in
+    let t' = Philox64.initialize_ctr ~counter:Uint64.(max_int, max_int, zero, max_int) ss |> Philox64.jump in
+    assert_bool "" ((Philox64.next_double t |> fst) <> (Philox64.next_double t' |> fst))
 
 
 let tests = [
