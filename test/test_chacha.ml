79ff5defe360b7a5c513de505f95594fad02f948
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2024-04-21 02:48:05 +0200

    Stylistic cleanup of ChaCha module

--- test/test_chacha.ml
+++ test/test_chacha.ml
@@ -1,5 +1,6 @@
 open OUnit2
 open Bitgen
+open Stdint
  
 
 let test_chacha_datasets _ =
@@ -12,17 +13,15 @@ let test_chacha_datasets _ =
 
 
 let test_full_init _ =
-    let open Stdint in
     let ss = SeedSequence.initialize [Uint128.of_int 12345] in
     let key = SeedSequence.generate_64bit_state 4 ss in
-    let full = ChaCha.initialize_full ~key:key ~counter:Uint64.(max_int, zero) ~rounds:4
-               |> ChaCha.next_uint64 |> fst |> Uint64.to_string in
-    let default = ChaCha.initialize ss |> ChaCha.next_uint64 |> fst |> Uint64.to_string in
+    let full = ChaCha.(initialize_full ~key:key ~counter:Uint64.(max_int, zero) ~rounds:4
+               |> next_uint64) |> fst |> Uint64.to_string in
+    let default = ChaCha.(initialize ss |> next_uint64) |> fst |> Uint64.to_string in
     assert_bool "stream should not be the same for different init counters" (full <> default)
 
 
 let test_invalid_round _ =
-    let open Stdint in
     let msg = "`rounds` must be a positive, even and > 2" in
     let key = SeedSequence.(initialize [Uint128.of_int 12345] |> generate_64bit_state 4) in
     let ctr = Uint64.(zero, zero) in
