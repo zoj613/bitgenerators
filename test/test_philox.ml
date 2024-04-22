9dbd7396a21791c79a87174b785bbf1cabf5f30d
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2024-04-22 12:57:45 +0200

    Add philox advance function

--- test/test_philox.ml
+++ test/test_philox.ml
@@ -33,8 +33,25 @@ let test_jump _ =
     assert_bool "" ((Philox64.next_double t |> fst) <> (Philox64.next_double t' |> fst))
 
 
+let test_advance _ =
+    let open Stdint in
+    (* since advance uses a 256-bit integer to advance, the equivalent number of
+       steps if manually calling next_uint64 would be 4 times larger than the
+       steps used to call advance. *)
+    let rec advance_n t = function
+        | 0 -> t
+        | i -> advance_n (Philox64.next_uint64 t |> snd) (i - 1) in
+    let t = SeedSequence.initialize [Uint128.of_int 12345]
+            |> Philox64.initialize_ctr ~counter:Uint64.(max_int, max_int, zero, zero) in
+    assert_equal
+        (Philox64.advance Uint64.(of_int 2, zero, zero, zero) t |> Philox64.next_uint64 |> fst |> Uint64.to_string)
+        (advance_n t (4 * 2) |> Philox64.next_uint64 |> fst |> Uint64.to_string)
+        ~printer:(fun x -> x)
+
+
 let tests = [
     "test Philox PNRG against groundtruth data" >:: test_philox_datasets;
     "test behaviour when counter is set" >:: test_counter_init;
     "test Philox jump function consistency" >:: test_jump;
+    "test Philox advance function correctness" >:: test_advance;
 ]
